026e8a4058faded0e885b92973ee43d7d291d19c
Kapu1178 <75460809+Kapu1178@users.noreply.github.com>
2022-05-27 14:34:12 -0400

    Updates printlist

--- printlist/printlist.nim
+++ printlist/printlist.nim
@@ -7,14 +7,13 @@ mylist[2] = "Baz"
 
 mylist.add("Lorem")
 
-proc printList(list : seq) = 
-    if list.len == 0:
-        echo($list[0])
-        return
-
+proc printList(list : seq) =
     var val = $list[0]
+    defer:  echo(val)
+
+    if list.len == 0:
+        return
     for item in 1 .. list.len-1:
         val &= fmt", {list[item]}"
-    echo(val)
 
 printList(mylist)
